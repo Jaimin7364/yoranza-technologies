8b2b04b30e1b07f45c7eadf6c6c82ce0c2cc4cca
Jaimin Raval <jaiminraval100@gmail.com>
2025-07-21 20:28:29 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,14 @@ import type { ReactNode } from 'react';
 
 export const metadata = {
   title: 'Yoranza Technologies',
-  description: 'Yoranza Technologies empowers businesses with cutting-edge IT solutions, cloud computing services, and dynamic web & app development. Experience innovation, scalability, and interactive digital transformation tailored for your success.',
+  description:
+    'Yoranza Technologies empowers businesses with cutting-edge IT solutions, cloud computing services, and dynamic web & app development. Experience innovation, scalability, and interactive digital transformation tailored for your success.',
   icons: {
     icon: '/icon.png',
+  },
+  metadataBase: new URL('https://www.yoranza.tech'), // ✅ update to www
+  alternates: {
+    canonical: '/', // this becomes https://www.yoranza.tech/
   },
 };
 
